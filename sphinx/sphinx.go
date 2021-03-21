dc06d6bc1ce9dddb601a8d334296a23fe94f07b6
Dennis Buduev <dennis.b@clubware.co.nz>
2021-03-22 11:53:37 +1300

    Add sphinx package.

--- sphinx/sphinx.go
+++ sphinx/sphinx.go
@@ -1,1 +1,39 @@
 package sphinx
+
+import "github.com/google/uuid"
+
+// BranchGroupContent represents a type aliasing a map
+// where key is a branch group UUID and value is a slice of UUIDs.
+// Each of latter UUIDs is a ID of a branch belonging to the branch group
+// designated by the key. The value contains all the branches of the branch group.
+type BranchGroupContent map[uuid.UUID][]uuid.UUID
+
+// BranchGroupsOfBranch represents a type aliasing a map
+// where key is a branch UUID and value is a slice of UUIDs.
+// Each of latter UUIDs is an ID of a branch group containing the branch
+// designated by the key. The value contains all the branch groups the branch belongs to.
+type BranchGroupsOfBranch map[uuid.UUID][]uuid.UUID
+
+func (m BranchGroupContent) Reverse() BranchGroupsOfBranch {
+	result := make(BranchGroupsOfBranch)
+	for group, branches := range m {
+		for _, branch := range branches {
+			if groups, ok := result[branch]; ok {
+				result[branch] = append(groups, group)
+			} else {
+				result[branch] = []uuid.UUID{group}
+			}
+		}
+	}
+	return result
+}
+
+func (m BranchGroupContent) Reverse1() BranchGroupsOfBranch {
+	result := make(BranchGroupsOfBranch)
+	for group, branches := range m {
+		for _, branch := range branches {
+			result[branch] = append(result[branch], group)
+		}
+	}
+	return result
+}
--- sphinx/sphinx_test.go
+++ sphinx/sphinx_test.go
@@ -1,1 +1,53 @@
 package sphinx
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/uuid"
+	"sort"
+	"testing"
+)
+
+func TestBranchGroupContent_Reverse(t *testing.T) {
+	trans := cmp.Transformer("Sort", func(in []uuid.UUID) []uuid.UUID {
+		out := append([]uuid.UUID(nil), in...) // Copy input to avoid mutating it
+		sort.Slice(out, func(i, j int) bool {
+			for k := 0; k < len(out[i]); k++ {
+				if out[i][k] != out[j][k] {
+					return out[i][k] < out[j][k]
+				}
+			}
+			return false
+		})
+		return out
+	})
+	var g = [...]uuid.UUID{uuid.New(), uuid.New(), uuid.New()}
+	var b = [...]uuid.UUID{uuid.New(), uuid.New(), uuid.New(), uuid.New()}
+
+	tests := []struct {
+		name string
+		m    BranchGroupContent
+		want BranchGroupsOfBranch
+	}{
+		{
+			name: "Simple case",
+			m: map[uuid.UUID][]uuid.UUID{
+				g[0]: {b[0], b[1], b[2]},
+				g[1]: {b[0], b[3]},
+			},
+			want: map[uuid.UUID][]uuid.UUID{
+				b[0]: {g[0], g[1]},
+				b[1]: {g[0]},
+				b[2]: {g[0]},
+				b[3]: {g[1]},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.m.Reverse1()
+			if diff := cmp.Diff(tt.want, got, trans); diff != "" {
+				t.Errorf("Reverse1() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
