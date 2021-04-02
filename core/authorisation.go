f0d41834ba7f37dff3daf4670fd3e966f8a2c256
Dennis Buduev <dennis.b@clubware.co.nz>
2021-04-02 13:28:22 +1300

    Implement WhereAuthorised function.

--- core/authorisation.go
+++ core/authorisation.go
@@ -12,6 +12,7 @@ func CreateAuthorisationCore(repository Repository) AuthorisationCore {
 	return AuthorisationCore{repository: repository}
 }
 
+// FindOpByName returns nil if operation is not found.
 func (ac *AuthorisationCore) FindOpByName(organisationId uuid.UUID, name string) *Operation {
 	ops, err := ac.repository.GetAllOperations(organisationId)
 	if err != nil {
@@ -27,20 +28,46 @@ func (ac *AuthorisationCore) FindOpByName(organisationId uuid.UUID, name string)
 	return nil
 }
 
-func (ac *AuthorisationCore) WhereAuthorised(organisationId, userId uuid.UUID, operation string) []UserRoleAssignment {
+// WhereAuthorised returns a slice of branch or branch group ids where the operation is authorised for the user.
+func (ac *AuthorisationCore) WhereAuthorised(organisationId, userId uuid.UUID, operation string) []uuid.UUID {
 	r := ac.repository
 
 	op := ac.FindOpByName(organisationId, operation)
 	if op == nil {
 		panic("operation not found " + operation)
 	}
+	// 1. op -> [role]
 	roles, err := r.GetRolesByOperation(organisationId, op.Id)
 	if err != nil {
 		panic(err)
 	}
+	// no roles supporting this operation. TODO: log with warning level.
 	if len(roles) == 0 {
 		return nil
 	}
 
-	panic("not implemented")
+	// 2. uid, role -> B, where B = [b|bg]
+	assignments, err := r.GetUserRolesAssignments(organisationId, userId)
+	if err != nil {
+		panic(err)
+	}
+
+	branches := make(map[uuid.UUID]struct{}, len(assignments))
+	for _, assignment := range assignments {
+		for _, role := range roles {
+			if role == assignment.RoleId {
+				branches[assignment.BranchId] = struct{}{}
+			}
+		}
+	}
+
+	if len(branches) == 0 {
+		return nil
+	} else {
+		result := make([]uuid.UUID, 0, len(branches))
+		for b := range branches {
+			result = append(result, b)
+		}
+		return result
+	}
 }
