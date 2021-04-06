789647caa86e908a153e0074ab69a77baf4edf43
Dennis Buduev <dennis.b@clubware.co.nz>
2021-04-07 11:27:55 +1200

    Add operations.{TransactionalInsert, GetEdges} test.

--- dygraph/operations_test.go
+++ dygraph/operations_test.go
@@ -3,23 +3,12 @@ package dygraph
 import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/google/go-cmp/cmp"
 	"github.com/google/uuid"
 	"reflect"
+	"sort"
 	"testing"
 )
-
-func GetClient() *dynamodb.DynamoDB {
-	// Create DynamoDB client
-	s := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-
-	return dynamodb.New(s, s.Config.WithEndpoint("http://localhost:8000"))
-}
-
-func CreateTestGraphClient() *Dygraph {
-	return CreateGraphClient(GetClient(), "test")
-}
 
 func TestGetNodes(t *testing.T) {
 	graphClient := CreateTestGraphClient()
@@ -42,4 +31,85 @@ func TestGetNodes(t *testing.T) {
 	if !reflect.DeepEqual(node, result[0]) {
 		t.Errorf("Expect %s, got %s", node, result[0])
 	}
+}
+
+func GenId(id uuid.UUID, b byte) uuid.UUID {
+	return uuid.NewSHA1(id, []byte{b})
+}
+
+func TestDygraph_TransactionalInsert(t *testing.T) {
+	graphClient := CreateTestGraphClient()
+
+	type args struct {
+		getItems func(id uuid.UUID) []Edge
+	}
+	tests := []struct {
+		name string
+		id   uuid.UUID
+		args args
+	}{
+		{
+			name: "Insert then retrieve two edges",
+			id:   uuid.New(),
+			args: args{
+				getItems: func(id uuid.UUID) []Edge {
+					return []Edge{
+						{
+							OrganisationId: id,
+							Id:             GenId(id, 1),
+							TargetNodeId:   GenId(id, 2),
+							TargetNodeType: "ROLE",
+							Tags:           []string{"tag1", "tag2"},
+							Data:           "data1",
+						},
+						{
+							OrganisationId: id,
+							Id:             GenId(id, 3),
+							TargetNodeId:   GenId(id, 4),
+							TargetNodeType: "ROLE",
+							Tags:           []string{"tag3", "tag4"},
+							Data:           "data2",
+						},
+					}
+				},
+			},
+		},
+	}
+	trans := cmp.Transformer("Sort", func(in []Edge) []Edge {
+		out := append([]Edge(nil), in...) // Copy input to avoid mutating it
+		sort.Slice(out, func(i, j int) bool {
+			return out[i].Data < out[j].Data
+		})
+		return out
+	})
+
+	for _, tt := range tests {
+		edges := tt.args.getItems(tt.id)
+		err := graphClient.TransactionalInsert(edges)
+		if err != nil {
+			t.Fatalf("Failed to insert edges %v with the error %v.", edges, err)
+		}
+
+		got, err := graphClient.GetEdges(tt.id, "ROLE")
+		if err != nil {
+			t.Fatalf("Failed to get edges. The error %v.", err)
+		}
+
+		if diff := cmp.Diff(edges, got, trans); diff != "" {
+			t.Errorf("TransactionalInsert() vs GetEdges() diff %v", diff)
+		}
+	}
+}
+
+func GetClient() *dynamodb.DynamoDB {
+	// Create DynamoDB client
+	s := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	return dynamodb.New(s, s.Config.WithEndpoint("http://localhost:8000"))
+}
+
+func CreateTestGraphClient() *Dygraph {
+	return CreateGraphClient(GetClient(), "test")
 }
